bbde96598b71c03f6ecf6fb82df8d9d73af9fa70
王云子 <85829302+wangyunzi@users.noreply.github.com>
2022-11-01 17:19:44 +0800

    Update Header.js

--- components/NavBar/Header.js
+++ components/NavBar/Header.js
@@ -1,4 +1,5 @@
 import { useEffect, useState, useRef } from 'react'
+import Image from 'next/image'
 import Link from 'next/link'
 import BLOG from '@/blog.config'
 import { lang } from '@/lib/lang'
